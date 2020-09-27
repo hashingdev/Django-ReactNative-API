76256f527981728a2c272b56618807574d399b5a
Muhammad Hammad <hammadarshad834@gmail.com>
2020-09-27 20:15:43 +0500

    Added Register API
    => added register API

--- ccore/urls.py
+++ ccore/urls.py
@@ -1,13 +1,6 @@
-from django.urls import include, path
-from rest_framework import routers
-from ccore import views
+from .views import RegisterAPI
+from django.urls import path
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/register/', RegisterAPI.as_view(), name='register'),
 ]
